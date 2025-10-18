b61c5a0490f344a5247c53ae29ab3ba1bd390962
CAPER TECH KENYA <caspertechke@gmail.com>
2025-10-18 23:54:44 +0300

    POWERED BY 𝖢𝖠𝖲𝖯𝖤𝖱 𝖳𝖤𝖢𝖧

--- app/layout.js
+++ app/layout.js
@@ -7,15 +7,17 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">
+      <body>
         <div className="min-h-screen relative">
           <AnimatedBackground />
-          <Navigation />
-          <main className="relative z-10">
-            {children}
-          </main>
-          <Footer />
-          <MusicPlayer />
+          <div className="relative z-10">
+            <Navigation />
+            <main>
+              {children}
+            </main>
+            <Footer />
+            <MusicPlayer />
+          </div>
         </div>
       </body>
     </html>
